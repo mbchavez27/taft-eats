3f7a64412f63a2620c4cae145179c7a96ad69287
mbchavez27 <epikstrykermaxchavez23@gmail.com>
2026-01-24 02:38:36 +0900

    feat: add search bar

--- components/organisms/navbar.tsx
+++ components/organisms/navbar.tsx
@@ -1,6 +1,8 @@
 import { useState } from "react";
 import { IoLocationOutline, IoHomeOutline } from "react-icons/io5";
 import { AiOutlineUser } from "react-icons/ai";
+import { IoIosSearch } from "react-icons/io";
+import SearchField from "components/molecules/searchfield";
 
 export default function NavBar() {
   const [isAuthendicated, setAuthentication] = useState(false);
@@ -21,13 +23,16 @@ export default function NavBar() {
             </div>
           </div>
         </section>
-        <section className="font-inter text-lg">
+        <section className="font-inter text-lg flex items-center gap-12">
+          <div className="flex items-center">
+            <SearchField />
+          </div>
           <div className="flex gap-6">
             <div className="bg-[#326F33] text-white p-2 rounded-full">
-              <IoLocationOutline size={24} />
+              <IoHomeOutline size={24} />
             </div>
             <div className="bg-[#326F33] text-white p-2 rounded-full">
-              <IoHomeOutline size={24} />
+              <IoLocationOutline size={24} />
             </div>
             {isAuthendicated ? (
               <>
