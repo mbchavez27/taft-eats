7378c82bc910bea20ee761dfbb621e4caf5b638d
mbchavez27 <epikstrykermaxchavez23@gmail.com>
2026-01-26 17:01:44 +0800

    feat: made it mobile responsive

--- app/features/establishments/components/organisms/establishment-card.tsx
+++ app/features/establishments/components/organisms/establishment-card.tsx
@@ -9,8 +9,8 @@ export default function EstablishmentsCard({
 }) {
   return (
     <>
-      <main className="flex flex-col gap-2">
-        <section className="bg-[#FFD24D] w-44 min-h-44 rounded-md flex flex-col justify-between p-2 gap-4 drop-shadow-2xl">
+      <main className="flex flex-col items-center gap-2">
+        <section className="bg-[#FFD24D] w-36 min-h-36 md:w-44 md:min-h-44 rounded-md flex flex-col justify-between p-2 gap-4 drop-shadow-2xl">
           <div className="flex justify-center">
             <div className="bg-[#D9D9D9] w-full h-24 rounded-md px-1 py-2">
               <div className="bg-[#848484] w-7 h-7 rounded-full"></div>
--- app/features/establishments/containers/establishments.tsx
+++ app/features/establishments/containers/establishments.tsx
@@ -6,10 +6,12 @@ export default function Establishments() {
     <>
       <div className="flex flex-col gap-6">
         <header>
-          <h1 className="font-climate text-4xl text-white">ESTABLISHMENTS</h1>
+          <h1 className="font-climate text-2xl md:text-4xl text-white">
+            ESTABLISHMENTS
+          </h1>
         </header>
         <ScrollArea className="h-175">
-          <main className="grid grid-cols-5 gap-8">
+          <main className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-5 gap-2 md:gap-12 lg:gap-8 justify-items-start ">
             <EstablishmentsCard name="La Tocaaaaaaaaaaaaaaa" rating={3} />
             <EstablishmentsCard name="La Toca" />
             <EstablishmentsCard name="La Toca" />
