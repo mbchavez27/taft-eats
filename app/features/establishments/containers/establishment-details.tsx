5ee10e474cf674c8b13029ebff54094e2e8ebdd5
mbchavez27 <epikstrykermaxchavez23@gmail.com>
2026-01-28 01:15:54 +0800

    feat: added write a review button

--- app/features/establishments/containers/establishment-details.tsx
+++ app/features/establishments/containers/establishment-details.tsx
@@ -15,6 +15,14 @@ export default function EstablishmentDetails() {
       <section className="flex flex-col gap-4">
         <ReviewSorts />
         <ReviewsList />
+        <section className="flex justify-end">
+          <button
+            className="bg-[#FFCB00] text-[#326F33] font-inter font-bold text-xl rounded-full px-6 py-2 hover:opacity-90
+           transition duration-75"
+          >
+            Write a Review
+          </button>
+        </section>
       </section>
     </main>
   )
