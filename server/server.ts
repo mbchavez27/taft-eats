8cfdf8a1e20db1af53dc6677f4241effb9100dbc
mbchavez27 <epikstrykermaxchavez23@gmail.com>
2026-02-16 11:13:25 +0800

    feat: server.ts and app.ts for server side

--- server/server.ts
+++ server/server.ts
@@ -1,1 +1,10 @@
-// Initializes and starts the server (app.listen)
+import dotenv from 'dotenv'
+import app from './src/app.ts'
+
+dotenv.config()
+
+const PORT = process.env.PORT || 3000
+
+app.listen(PORT, () => {
+  console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
+})
--- server/src/app.ts
+++ server/src/app.ts
@@ -1,1 +1,7 @@
-// Express app configuration (no listen)
+import express from 'express'
+
+const app = express()
+
+app.use(express.json())
+
+export default app
