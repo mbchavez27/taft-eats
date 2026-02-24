aa415d8cbd7fdb23ec2ca2ad99f12a21ac8fec68
mbchavez27 <epikstrykermaxchavez23@gmail.com>
2026-02-24 16:36:55 +0800

    feat: implement the remaining user services methods

--- client/app/features/auth/services/auth.services.ts
+++ client/app/features/auth/services/auth.services.ts
@@ -1,12 +1,13 @@
 import type {
   AuthResponse,
   CreateUserDTO,
+  LoginDTO,
 } from '~/features/users/types/user.types'
 
 const API_BASE_URL = import.meta.env.VITE_API_BASE_URL
 
 export const AuthService = {
-  regiser: async (userData: CreateUserDTO): Promise<AuthResponse> => {
+  register: async (userData: CreateUserDTO): Promise<AuthResponse> => {
     const response = await fetch(`${API_BASE_URL}/auth/register`, {
       method: 'POST',
       headers: {
@@ -26,5 +27,36 @@ export const AuthService = {
     }
 
     return data
+  },
+
+  login: async (credentials: LoginDTO): Promise<AuthResponse> => {
+    const response = await fetch(`${API_BASE_URL}/auth/login`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(credentials),
+    })
+    const data: AuthResponse = await response.json()
+
+    if (!response.ok) {
+      throw new Error(
+        data.error || 'Login failed, please check your credentials',
+      )
+    }
+
+    if (data.token) {
+      localStorage.setItem('token', data.token)
+    }
+
+    return data
+  },
+
+  logout: () => {
+    localStorage.removeItem('token')
+  },
+
+  getToken: () => {
+    return localStorage.getItem('token')
   },
 }
