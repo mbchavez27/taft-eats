9844c30516c13d6bfd9f67287dd86cd32e65943d
alexgnzls <alexandragayleg@gmail.com>
2026-03-15 14:24:56 +0800

    feat: added empty reviews UI

--- client/app/features/reviews/containers/establishment-reviews.tsx
+++ client/app/features/reviews/containers/establishment-reviews.tsx
@@ -7,9 +7,11 @@ type SortValue = 'newest' | 'oldest' | 1 | 2 | 3 | 4 | 5
 export default function EstablishmentReviews({
   onReply,
   restaurantId,
+  restaurantName,
 }: {
   onReply: () => void
   restaurantId?: number
+  restaurantName?: string
 }) {
   const [sort, setSort] = useState<SortValue>('newest')
 
@@ -17,7 +19,12 @@ export default function EstablishmentReviews({
     <main className="flex flex-col gap-4">
       <ReviewSorts onSortChange={setSort} />
       {restaurantId ? (
-        <ReviewsList onOpenForms={onReply} restaurantId={restaurantId} sort={sort} />
+        <ReviewsList
+          onOpenForms={onReply}
+          restaurantId={restaurantId}
+          sort={sort}
+          restaurantName={restaurantName}
+        />
       ) : (
         <div className="text-gray-400">Loading reviews...</div>
       )}
