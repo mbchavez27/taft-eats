15dfd415f1267a236968b32ae076f135974fa8e8
mbchavez27 <epikstrykermaxchavez23@gmail.com>
2026-03-25 03:51:39 +0800

    feat: add docker compose

--- server/src/app.ts
+++ server/src/app.ts
@@ -31,7 +31,7 @@ app.use('/uploads', express.static('uploads'))
 
 app.use(
   cors({
-    origin: 'http://localhost:5173',
+    origin: ['http://152.42.227.161', 'http://localhost:5173'],
     credentials: true,
   }),
 )
